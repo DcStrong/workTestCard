deafa3e4f6deb3adbe2d6f15fe9edb41e56c4a8f
dc_strong <dc_strong@mail.ru>
2019-06-24 01:10:56 +0300

    frontpage

--- src/js/script.js
+++ src/js/script.js
@@ -1,3 +1,25 @@
 ;(function($) {
-  simpleslider.getSlider();
+  // slider
+  var slider = simpleslider.getSlider({
+    prop: 'opacity',
+    unit: '',
+    init: 0,
+    show: 1,
+    end: 0,
+    duration: 1,
+    delay: 5
+  });
+  $('.slider__prev').click(function(){
+    slider.prev();
+  });
+  $('.slider__next').click(function(){
+    slider.next();
+  });
+
+  $('[data-popup-toggler]').click(function(){
+    $('[data-popup]').addClass('show');
+  });
+  $('.popup__close').click(function(){
+    $('[data-popup]').removeClass('show');
+  })
 })(jQuery);
